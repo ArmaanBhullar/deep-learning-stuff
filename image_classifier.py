2c38d204c79db2ad5de1e27da46fbfc790517f62
Armaan Bhullar <jhaniria@gmail.com>
2021-07-20 15:32:25 +0530

    Add pre commit hooks

--- image_classifier.py
+++ image_classifier.py
@@ -10,17 +10,18 @@ resnet = torchvision.models.resnet101(pretrained=True)
 # Put the network in eval model, If we forget to do that, some pretrained models, like batch normalization and dropout,
 # will not produce meaningful answers, just because of the way they work internally.
 # Now that eval has been set, we’re ready for inference:
+
 resnet.eval()
 
 # Create a preprocessor
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )]) # Predefined values as used in training of the Resnet model
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)  # Predefined values as used in training of the Resnet model
 
 img = Image.open("./data/dog_1.jpg")
 img_t = preprocess(img=img)
@@ -38,4 +39,6 @@ with open("./data/imagenet_classes.txt", "rt") as fp:
 # Find the highest value's index
 _, index = torch.max(out, 1)
 percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-print(f"Predicted class = {labels[index[0]]}, Confidence = {percentage[index[0]].item()} %")
+print(
+    f"Predicted class = {labels[index[0]]}, Confidence = {percentage[index[0]].item()} %"
+)
